fa1f42791940855fcebc1844ba20c133dfc8b914
Trevor May <trevormay@Trevors-MacBook-Pro.local>
2019-06-09 16:01:36 -0600

    2nd commit

--- Bamazon/bamazonCustomer.js
+++ Bamazon/bamazonCustomer.js
@@ -14,20 +14,56 @@ var connection = mysql.createConnection({
 
     //password
     password: "root",
-    database: "bamazonDB"
+    database: "bamazon_DB"
 });
 
 //for errors
 connection.connect(function(err) {
     if(err) throw err;
-    console.log("connect as id " + connection.threadId + "\n");
-    runSearch();
+    console.log("connect as id " + connection.threadId);
+    
 });
 
-function runSearch() {
+
+//prompt the questions 
+function purchasePrompt() {
     inquirer
-        .prompt({
-            message: "What is the ID of your purchase?",
-            message: "How many units would you like to buy?"
-        })
-}
+        .prompt([
+            {
+                name: "ID",
+                type: "input",
+                message:"Please enter Item ID you like to purchase.",
+                filter:Number
+            },
+
+            {
+                name: "Quantity",
+                type: "input",
+                message:"How many items would you like to purchase?",
+                filter:Number
+            },
+        ]).then(function(answers){
+            var quantityNeeded = answers.Quantity;
+            var IDrequested = answers.ID;
+            purchaseOrder(IDrequested, quantityNeeded);
+        });
+       };
+
+       function purchaseOrder(ID, amtNeeded){
+        connection.query('Select * FROM products WHERE item_id = ' + ID, function(err,res){
+            if(err){console.log(err)};
+            if(amtNeeded <= res[0].stock_quantity){
+                var totalCost = res[0].price * amtNeeded;
+                console.log("Good news your order is in stock!");
+                console.log("Your total cost for " + amtNeeded + " " +res[0].product_name + " is " + totalCost + " Thank you!");
+    
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+            } else{
+                console.log("Insufficient quantity! " + res[0].product_name + " to complete your order.");
+            };
+            
+        });
+        
+    };
+
+    purchasePrompt();
--- Bamazon/bamazonDB.sql
+++ Bamazon/bamazonDB.sql
@@ -5,16 +5,27 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products ( 
-item_id INTEGER AUTO_INCREMENT NOT NULL,
+id int AUTO_INCREMENT NOT NULL,
 product_name VARCHAR(20) NULL,
 department_name VARCHAR(20) NULL,
 price DECIMAL (10,2) NULL,
-stock_quantity INT NULL,
+stock_quantity int NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO products (product, price, stock_quantity)
-VALUES ("chapstick", "beauty", 1.50, 500), ("waterbottle", "health", 7.00, 120), ("straw hat", "fashion", 15.00, 30), ("super-man cape", "apparel", 13.00, 12), ("television", "electronics", 299.00, 7), ("bose speaker", "electronics", 179.00, 4), ("dragon statue", "decor", 129.00, 3), ("wasabi peas", "food", 9.00, 14), ("world tapestry", "decor", 1100.00, 17);
+Select * FROM products;
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES (1, "chapstick", "beauty", 1.50, 500),
+ (2, "waterbottle", "health", 7.00, 120),
+ (3, "straw hat", "fashion", 15.00, 30),
+ (4, "super-man cape", "apparel", 13.00, 12),
+ (5, "television", "electronics", 299.00, 7),
+ (6, "bose speaker", "electronics", 179.00, 4),
+ (7, "dragon statue", "decor", 129.00, 3),
+ (8, "wasabi peas", "food", 9.00, 14),
+ (9, "world tapestry", "decor", 1100.00, 17),
+ (10, "notebook", "stationary", 17.00, 20);
 
 
 
